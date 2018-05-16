31d9f1a786f1f4a2754b5e788ed5cda39009de96
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-16 00:58:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,8 +34,8 @@ class Student
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
-    
-     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0]
+
+     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0].flatten
   end
 
   def self.create(id)
